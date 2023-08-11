2a98ae8e9f35819e2fe303a12233f7b1075e773f
ko-nee <konemas10@gmail.com>
2023-08-10 17:39:22 -0700

    hello world commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("size of char; %lu byte(s)\n"), (unsigned long)sizeof(a));
-printf("size of int; %lu byte(s)\n"), (unsigned long)sizeof(b));
-printf("size of long int; %lu byte(s)\n"), (unsigned long)sizeof(c));
-printf("size of long long int; %lu byte(s)\n"), (unsigned long)sizeof(d));
-printf("size of float; %lu byte(s)\n"), (unsigned long)sizeof(e));
+printf("size of char; %d byte(s)\n"), (unsigned long)sizeof(a));
+printf("size of int; %d byte(s)\n"), (unsigned long)sizeof(b));
+printf("size of long int; %d byte(s)\n"), (unsigned long)sizeof(c));
+printf("size of long long int; %d byte(s)\n"), (unsigned long)sizeof(d));
+printf("size of float; %d byte(s)\n"), (unsigned long)sizeof(e));
 return (0);
 }
