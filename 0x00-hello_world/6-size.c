26aff9c782ad632f9c49c107c75e7802d1f51a4a
ko-nee <konemas10@gmail.com>
2023-08-10 17:45:43 -0700

    hello world commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,14 @@
 /**
  * Main - program printing sizes of various types
  *
- * Return: 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-
-printf("size of char; %d byte(s)\n"), (unsigned long)sizeof(a));
-printf("size of int; %d byte(s)\n"), (unsigned long)sizeof(b));
-printf("size of long int; %d byte(s)\n"), (unsigned long)sizeof(c));
-printf("size of long long int; %d byte(s)\n"), (unsigned long)sizeof(d));
-printf("size of float; %d byte(s)\n"), (unsigned long)sizeof(e));
-return (0);
+	printf("size of char; %d byte(s)\n"), (sizeof(char));
+	printf("size of int; %d byte(s)\n"), (sizeof(int));
+	printf("size of long int; %d byte(s)\n"), (sizeof(long int));
+	printf("size of long long int; %d byte(s)\n"), (sizeof(long long int));
+	printf("size of float; %d byte(s)\n"), (sizeof(float));
+	return (0);
 }
