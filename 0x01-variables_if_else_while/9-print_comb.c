644e7014685fa6cb761ee427226d1aa1496d1861
ko-nee <konemas10@gmail.com>
2023-08-11 18:32:32 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints all possible combinations of
+ * single-digit numbers separated by ", ".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 9; num1++)
+{
+for (num2 = num1; num2 <= 9; num2++)
+{
+putchar(num1 + '0');
+putchar(num2 + '0');
+if (num1 != 9 || num2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
