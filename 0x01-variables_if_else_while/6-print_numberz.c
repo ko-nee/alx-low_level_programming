76c5f2b50ed3d57ef39382578d9f74c540e09541
ko-nee <konemas10@gmail.com>
2023-08-11 18:17:49 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,18 +8,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void))
 {
-putchar('0' + 0);
-putchar('0' + 1);
-putchar('0' + 2);
-putchar('0' + 3);
-putchar('0' + 4);
-putchar('0' + 5);
-putchar('0' + 6);
-putchar('0' + 7);
-putchar('0' + 8);
-putchar('0' + 9);
+int number;
+
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
 putchar('\n');
 return (0);
 }
