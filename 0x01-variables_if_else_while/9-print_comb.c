52a7b6f2989fdd3ed095dbe11c069a0d04ee020c
ko-nee <konemas10@gmail.com>
2023-08-11 18:39:29 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 int num1, num2;
-
 for (num1 = 0; num1 <= 9; num1++)
 {
 for (num2 = num1; num2 <= 9; num2++)
@@ -20,8 +19,11 @@ putchar(num1 + '0');
 putchar(num2 + '0');
 if (num1 != 9 || num2 != 9)
 {
+if (!(num1 == 8 && num2 == 9))
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
