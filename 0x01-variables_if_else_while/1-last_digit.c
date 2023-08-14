46c3909cd6c28bdff025793365f4d85f79bfe15d
ko-nee <konemas10@gmail.com>
2023-08-14 13:00:31 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,7 @@
 
 /**
  * main - Entry point 
- * Description - print whether the number stored
- * in the variable n is positive or negative
+ * Description: print the last digit of the number stored in the variable n
  *
  * Result =  0 (Success)
  */
