071b693f933618db050f3fd6ef55d6051db90563
ko-nee <konemas10@gmail.com>
2023-08-17 15:35:13 -0700

    0x04-more_functions_nested_loops commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main: checks the code
- * @c: checks uppercase character
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
  *
- * Return: (1) or (0)
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
