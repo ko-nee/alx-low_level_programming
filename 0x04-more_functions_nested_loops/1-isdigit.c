85d0b4d4a590a6aa7442d0e9ff0312c5428520ab
ko-nee <konemas10@gmail.com>
2023-08-17 16:17:07 -0700

    0x04-more_functions_nested_loops commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isdigit checks for a digit (0 through 9)
- * @c: the character to be checked
+ * main: _isdigit checks for a digit
+ * @c: the character is a  digit
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - mutliplies 2 integers
+ * @x: int x
+ * @y: int y
+ *
+ * Return: product of multiplication
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
