e812d81cf7f2ce75e02b5dedfb17bedce68beda1
ko-nee <konemas10@gmail.com>
2023-08-17 16:31:04 -0700

    0x04-more_functions_nested_loops commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers- print numbers from 0-9
+ * followed by a new line
+ *
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
