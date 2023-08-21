c5690fdc05d572f4a198cb2f75fedb28fe007bb0
ko-nee <konemas10@gmail.com>
2023-08-21 16:18:46 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,73 +1,26 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_rev - imprime en reversa
+ * @s: string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-/**
- * _strlen - Calculate the length of a string.
- * @s: The input string.
- *
- * Return: The length of the string.
- */
-int _strlen(char *s)
-{
-    int len = 0;
-
-    while (s[len] != '\0')
-        len++;
-
-    return len;
-}
-
-/**
- * _puts - Print a string followed by a new line to stdout.
- * @str: The input string.
- */
-void _puts(char *str)
-{
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-
-    _putchar('\n'); // Print a new line after the string
-}
-
-/**
- * print_rev - Print a string in reverse followed by a new line.
- * @s: The input string.
+ * Return: 0
  */
 void print_rev(char *s)
 {
-    int len = _strlen(s);
+	int longi = 0;
+	int o;
 
-    for (int i = len - 1; i >= 0; i--)
-        _putchar(s[i]);
-
-    _putchar('\n'); // Print a new line after the reversed string
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+	}
+	s--;
+	for (o - longi; o > 0; o--)
+	{
+	_putchar(*s);
+	s--;
 }
-
-int main(void)
-{
-    char *message = "Hello, world!";
-    
-    _puts("Original message:");
-    _puts(message);
-    
-    _puts("Reversed message:");
-    print_rev(message);
-
-    return 0;
+	putchar('\n');
 }
