4f8b84447fd55659a199a9117a3a50ec92bfeb7c
ko-nee <konemas10@gmail.com>
2023-08-21 15:25:40 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,14 @@
  * swap_int - Swap the values of two integers.
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
+ *
+ *Return (0)
  */
 void swap_int(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+
+int main(void)
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0'\n)
+	    len++;
+
+	return (len);
+}
