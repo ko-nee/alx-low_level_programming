e7d3a3998429cc11a938634a62c33d25104ed9dc
ko-nee <konemas10@gmail.com>
2023-08-21 17:28:21 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+	int numbers[] = {1, 2, 3, 4, 5};
+	int num_elements = sizeof(numbers) / sizeof(numbers[0]);
+
+	printf("Array elements: ");
+	print_array(numbers, num_elements);
+
+	return 0;
+}
