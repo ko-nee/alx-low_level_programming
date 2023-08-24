5c505cc2c96f40ca3878f3367c4a29786fcc1858
ko-nee <konemas10@gmail.com>
2023-08-24 14:37:02 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -22,20 +22,3 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
-
-int main(void)
-{
-	char str1[] = "apple";
-	char str2[] = "banana";
-
-	int result = _strcmp(str1, str2);
-
-	if (result < 0)
-		printf("str1 is less than str2\n");
-	else if (result > 0)
-		printf("str1 is greater than str2\n");
-	else
-		printf("str1 is equal to str2\n");
-
-	return (0);
-}
