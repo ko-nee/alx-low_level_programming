7809c89b99b8c23beca483f99cba2eeeaae2ccb8
ko-nee <konemas10@gmail.com>
2023-08-28 13:09:57 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,23 +5,20 @@
  * @s: Pointer to the string to be searched.
  * @accept: Pointer to the string containing bytes to search for.
  *
- * Return: A pointer to the byte in @s that matches one of the bytes in @accept,
- *         or NULL if no such byte is found.
+ * Return: Always 0 (Success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-    while (*s)
-    {
-        while (char *a = accept; *a != '\0'; a++)
-        {
-            if (*s == *a)
-            {
-                return s;
-            }
-        }
-        s++;
-    }
+	int i;
 
-    return NULL;
+	while (*s)
+	{
+	for (i = 0, accept[i], i++)
+	{
+		if (*s == accept[i])
+		return s;
+		}
+	}
+	s++;
 }
-
