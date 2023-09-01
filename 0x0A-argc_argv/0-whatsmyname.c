e707dc528105939a9a0c6183c89dacf049a24b4c
ko-nee <konemas10@gmail.com>
2023-09-01 14:32:38 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The no. of cl arguments
+ * @argv: An array containing the cl arguments
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    printf("%s\n", *argv);
+        return (0);
+}
