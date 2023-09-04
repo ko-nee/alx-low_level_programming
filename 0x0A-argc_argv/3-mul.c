8ce6f7d9dab55a797f0c125432c72f3062fdd7ba
ko-nee <konemas10@gmail.com>
2023-09-04 16:41:13 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 	return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
