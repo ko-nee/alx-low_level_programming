f3b6628d31f66779b271e8bc8776db4497cb592f
ko-nee <konemas10@gmail.com>
2023-09-05 15:15:39 -0700

    0x09-static_libraries

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -43,11 +43,11 @@ int **alloc_grid(int width, int height)
 	return (NULL);
 	}
 
-        for (j = 0; j < width; j++)
-        {
-            grid[i][j] = 0;
-        }
-    }
+	for (j = 0; j < width; j++)
+	{
+	grid[i][j] = 0;
+	}
+	}
 
-    return (grid);
+	return (grid);
 }
