597315600f1efeee6c036d68ab9281b29b1eb875
ko-nee <konemas10@gmail.com>
2023-09-11 15:24:00 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_NAMED_SIZE_H
+#define MAKRO_NAMED_SIZE_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACROSUM_H
+#define FUNCTION_LIKE_MACROSUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
