ebc6b1a5fd86cedb6947ea9c00e2adf9780afae3
ko-nee <konemas10@gmail.com>
2023-09-26 04:06:28 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,15 +4,11 @@ void my_constructor(void) __attribute__((constructor));
 
 
 /**
- * first - prints a sentence pre main
+ * first - prints a sentence before main
  * my_constructor - Function executed before main.
  */
-
-
 void first(void)
 {
-
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
