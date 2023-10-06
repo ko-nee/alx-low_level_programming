96f2d0dfbed00ace3856e0e46cd5925fb3f252a5
ko-nee <konemas10@gmail.com>
2023-10-06 12:03:03 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,16 +24,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 	return (-1);
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 	ssize_t bw = write(fd, text_content, strlen(text_content));
+
 	if (bw == -1)
 	{
 	close(fd);
 	return (-1);
 	}
 	}
-
 	close(fd);
 	return (1);
 }
